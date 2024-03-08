4f3683be9eca47746acf524755af792ece7657d5
Stefan Lazarevic <stefan.lazarevic@cm.com>
2024-03-08 17:18:41 +0100

    ang app udemy

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -3,7 +3,25 @@ import { Component } from "@angular/core";
 @Component({
   selector: 'app-server',
   templateUrl: './server.component.html',
+  styles: [`
+  .online {
+    color: white;
+  }
+  `]
 })
 export class ServerComponent {
+  serverID = 10;
+  status = 'offline';
 
+  constructor(){
+    this.status = Math.random() > 0.5 ? 'online' : 'offline';
+  }
+
+  getServerStatus() {
+    return this.status;
+  }
+
+  getColor(){
+    return this.status === 'online' ? 'green' : 'red';
+  }
 }
